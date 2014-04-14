a8c393e37b690e3f6a5677f96525f9b9e683d6b2
Cedric Porter <cedricporter@gmail.com>
2014-04-15 00:32:20 +0800

    add test demo

--- main.lua
+++ main.lua
@@ -1,18 +1,26 @@
--- Author: Hua Liang[Stupid ET] <et@everet.org>
 require 'StateMachine'
 
 function main()
     local fsm = StateMachine.new()
-    -- （注：和demo不同的是，demo采用组件形式完成的初始化）
-    fsm:setupState({
+    fsm = fsm:setupState({
             initial = "green",
             events  = {
                 {name = "warn",  from = "green",  to = "yellow"},
                 {name = "panic", from = "green",  to = "red"   },
                 {name = "calm",  from = "red",    to = "yellow"},
                 {name = "clear", from = "yellow", to = "green" },
-            }
+            },
+            callbacks = {
+                onbeforestart = function(event) print("[FSM] STARTING UP") end,
+                onstart       = function(event) print("[FSM] READY") end,
+                onbeforewarn  = function(event) print("[FSM] START   EVENT: warn!") end,
+                onbeforepanic = function(event) print("[FSM] START   EVENT: panic!") end,
+                onbeforecalm  = function(event) print("[FSM] START   EVENT: calm!") end,
+                onbeforeclear = function(event) print("[FSM] START   EVENT: clear!") end,
+                onwarn        = function(event) print("[FSM] FINISH  EVENT: warn!") end,
+            },
     })
+    fsm:doEvent("warn", "some msg")
 end
 
 main()    
